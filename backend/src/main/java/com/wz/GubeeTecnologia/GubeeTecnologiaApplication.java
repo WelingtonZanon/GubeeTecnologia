f9c263de68dfc20367aa1761f036b8c20af57f8c
Welington <welington_zanon@hotmail.com>
2021-10-01 11:00:20 -0300

    create backend

---
+++ backend/src/main/java/com/wz/GubeeTecnologia/GubeeTecnologiaApplication.java
@@ -0,0 +1,13 @@
+package com.wz.GubeeTecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GubeeTecnologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GubeeTecnologiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wz/GubeeTecnologia/GubeeTecnologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wz.GubeeTecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GubeeTecnologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
