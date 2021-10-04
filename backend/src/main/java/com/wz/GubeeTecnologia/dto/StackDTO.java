b61f5c3ea0b65200d164ba3d80be616ef9b91894
Welington <welington_zanon@hotmail.com>
2021-10-04 19:03:12 -0300

    Dev Postgre test

--- backend/src/main/java/com/wz/GubeeTecnologia/dto/StackDTO.java
+++ backend/src/main/java/com/wz/GubeeTecnologia/dto/StackDTO.java
@@ -5,8 +5,10 @@ import java.io.Serializable;
 import com.wz.GubeeTecnologia.entities.Stack;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class StackDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
--- backend/src/main/java/com/wz/GubeeTecnologia/dto/TargetMarketDTO.java
+++ backend/src/main/java/com/wz/GubeeTecnologia/dto/TargetMarketDTO.java
@@ -5,8 +5,10 @@ import java.io.Serializable;
 import com.wz.GubeeTecnologia.entities.TargetMarket;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class TargetMarketDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
